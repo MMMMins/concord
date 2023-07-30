4cdd2f826920bbbb178618aa6f90405d01ebe8d5
min <tommp1055@gmail.com>
2023-07-30 22:14:11 +0900

    구조 세팅중

--- app/src/main/java/com/hackathon/concord/View/MainActivity.java
+++ app/src/main/java/com/hackathon/concord/View/MainActivity.java
@@ -1,16 +1,49 @@
 package com.hackathon.concord.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hackathon.concord.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView nav;
+    InfoFragment infoFragment;
+    VariousFragment variousFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoFragment = new InfoFragment();
+        variousFragment = new VariousFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.bottomNav, infoFragment).commit();
+
+        nav = findViewById(R.id.bottomNav);
+        nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.tab1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.bottomNav, infoFragment).commit();
+                        return true;
+                    case R.id.tab2:
+                        return true;
+                    case R.id.tab3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.bottomNav, variousFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 }
