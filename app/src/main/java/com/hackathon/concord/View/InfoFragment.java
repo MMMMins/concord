b72d3f7d0c2e09b2bd4d1dce80f6b9e3d860fb4b
min <tommp1055@gmail.com>
2023-08-11 07:12:49 +0900

    리스트 클릭시 화면전환 구현

--- app/src/main/java/com/hackathon/concord/View/InfoFragment.java
+++ app/src/main/java/com/hackathon/concord/View/InfoFragment.java
@@ -3,18 +3,21 @@ package com.hackathon.concord.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.hackathon.concord.Model.UserModel;
 import com.hackathon.concord.Model.UserPetInfoModel;
 import com.hackathon.concord.R;
 
-public class InfoFragment extends Fragment {
+public class InfoFragment extends Fragment implements AdapterView.OnItemClickListener {
     UserPetInfoModel userPetInfoModel;
     ListView listView;
     InfoLiveViewAdapter adpater;
@@ -37,13 +40,20 @@ public class InfoFragment extends Fragment {
         //adpater 선언
         adpater = new InfoLiveViewAdapter();
         listView = (ListView) v.findViewById(R.id.petListView);
-        listView.setAdapter(adpater);
 
         adpater.addItem(userPetInfoModel.getPets());
+        listView.setAdapter(adpater);
 
         //값 변경 알림
         adpater.notifyDataSetChanged();
-
+        listView.setOnItemClickListener(this);
         return v;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(getActivity(), PetInfoActivity.class);
+        intent.putExtra("pet_info", userPetInfoModel.getPets().get(position));
+        startActivity(intent);
     }
 }
