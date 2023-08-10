7b10ef4bd1b2fdc0ab516946f9f5fa54246c45e1
min <tommp1055@gmail.com>
2023-08-11 07:18:14 +0900

    로그인 사용자의 모든정보 요청

--- app/src/main/java/com/hackathon/concord/viewModel/InfoViewModel.java
+++ app/src/main/java/com/hackathon/concord/viewModel/InfoViewModel.java
@@ -1,26 +1,17 @@
 package com.hackathon.concord.viewModel;
 
-import android.util.Base64;
-import android.util.Log;
-
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-
-import com.hackathon.concord.Model.LoginDataSource;
-import com.hackathon.concord.Model.PetModel;
+import com.hackathon.concord.API.RetrofitService;
 import com.hackathon.concord.Model.UserPetInfoModel;
 
-import java.io.IOException;
+import android.util.Log;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+
 public class InfoViewModel extends ViewModel implements Serializable {
     private MutableLiveData<UserPetInfoModel> info = new MutableLiveData<>();
 
@@ -36,28 +27,15 @@ public class InfoViewModel extends ViewModel implements Serializable {
             @Override
             public void onResponse(Call<UserPetInfoModel> call, Response<UserPetInfoModel> response) {
                 if (response.isSuccessful()) {
+                    Log.e("에러","a");
                     UserPetInfoModel userPetInfoModel = response.body();
-                    List<PetModel> tempPetList = new ArrayList<>();
                     if (userPetInfoModel != null) {
-                        for(PetModel petModel : userPetInfoModel.getPets()){
-                            if (petModel.getPetImage() != null) {
-                                petModel.setPetImage(petModel.getPetImage().replace("/pet_image/", ""));
-                                petModel.setPetImage(petModel.getPetImage().replace("%2B", "+"));
-                                petModel.setPetImage(petModel.getPetImage().replace("%3D", "="));
-//
-//                                Log.e("", String.valueOf(petModel.getPetImage().length()));
-//                                byte[] decodedImage = Base64.decode(petModel.getPetImage(), Base64.DEFAULT);
-//
-//                                Log.e("ee", String.valueOf(decodedImage.length));
-//                                petModel.setImageByte(decodedImage);
-                                tempPetList.add(petModel);
-                            }
-                        }
-                        userPetInfoModel.setPets(tempPetList);
                         info.setValue(userPetInfoModel);
                     }
                 } else {
                     // API 호출에 실패한 경우
+                    Log.e("에러","b");
+
                 }
             }
 
